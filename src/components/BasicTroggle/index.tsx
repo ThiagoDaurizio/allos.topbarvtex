0f61e35d72ee098ba277208f598a75802bf367e8
Thiago Daurizio <Thiago_daurizio@hotmail.com>
2023-11-08 17:05:08 -0300

    space test

--- src/components/BasicTroggle/index.tsx
+++ src/components/BasicTroggle/index.tsx
@@ -10,7 +10,7 @@ const BasicTroggle = ({ troggle, set_troggle }: IProps) => {
     <div onClick={() => set_troggle(!troggle)}
     className={`h-[30px] min-w-[60px] flex items-center rounded-full cursor-pointer ${!troggle ? 'bg-rose-500' : 'bg-green-500'}`}
     >
-    <span
+    <span 
       className={`bg-gray-300 h-[26px] w-[26px] block rounded-full transform ${!troggle ? 'translate-x-[2px]' : 'translate-x-[32px]'} transition-transform duration-200`}
       />
   </div>
